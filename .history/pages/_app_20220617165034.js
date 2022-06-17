a76fbfdd9d02495d65557dca961db3bca9964a5d
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-06-17 16:51:04 -0400

    Head modified

--- .history/pages/_app_20220617165034.js
+++ .history/pages/_app_20220617165034.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <div style={{ padding: '100px' }}>
       <Head>
-        <title>Lens Social Media</title>
+        <title>Capital Bank</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
